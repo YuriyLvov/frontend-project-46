f9995f18c6515714a7f9ecadbcef0db16a035514
Yuriy Lvov <y.lvov.ca@gmail.com>
2023-02-26 13:46:06 -0500

    step 3

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/YuriyLvov/frontend-project-46/workflows/hexlet-check/badge.svg)](https://github.com/YuriyLvov/frontend-project-46/actions)
+
+### step 3
+[![asciicast](https://asciinema.org/a/ryDkO5Q2xJfxMGjhBPVNETKlp.svg)](https://asciinema.org/a/ryDkO5Q2xJfxMGjhBPVNETKlp)
--- src/gendiff.js
+++ src/gendiff.js
@@ -1,7 +1,10 @@
+import fs from 'fs';
+import path from  'path';
+import process from  'process';
 import { Command } from 'commander';
+import lodash from 'lodash';
 
-const runGenDiff = () => {
-
+const genDiff = () => {
   const cli = new Command();
 
   cli
@@ -10,9 +13,54 @@ const runGenDiff = () => {
     .option('-f, --format <type>', 'output format')
     .arguments('<filepath1>')
     .arguments('<filepath2>')
+    .action((filepath1, filepath2) => {
+      const cwd = process.cwd();
+      const file1path = path.resolve(cwd, filepath1);
+      const file2path = path.resolve(cwd, filepath2);
+
+      const file1 = fs.readFileSync(file1path, {encoding: 'utf-8'});
+      const file2 = fs.readFileSync(file2path, {encoding: 'utf-8'});
+
+      const file1json = JSON.parse(file1);
+      const file2json = JSON.parse(file2);
+
+      const fileKeys = [...lodash.keys(file1json), ...lodash.keys(file2json)];
+      const uniqueKeys = lodash.uniq(fileKeys).sort();
+
+      let comparison = '';
+
+      for (const key of uniqueKeys) {
+        const value1 = file1json[key];
+        const value2 = file2json[key];
+
+        if (value1 !== undefined && value2 === undefined) {
+          comparison += `- ${key}: ${value1}\n`;
+          continue;
+        } 
+        
+        if (value2 !== undefined && value1 === undefined) {
+          comparison += `+ ${key}: ${value2}\n`;
+          continue;
+        }
+
+        if (value1 === value2) {
+          comparison += `  ${key}: ${value1}\n`;
+          continue;
+        }
+
+        comparison += `- ${key}: ${value1}\n`;
+        comparison += `+ ${key}: ${value2}\n`;
+      }
+
+      const result = `{\n${comparison}\n}`;
+
+      console.log(result);
+
+      return result;
+    })
     .version('0.0.1');
 
   cli.parse();
 };
 
-export default runGenDiff;
+export default genDiff;
