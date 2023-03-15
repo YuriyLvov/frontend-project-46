80bf0b7f37802bcedd0b4520f2c39d2a4f2517b8
Yuriy Lvov <y.lvov.ca@gmail.com>
2023-03-14 20:38:52 -0400

    fix lint

--- src/formatter/stylish.js
+++ src/formatter/stylish.js
@@ -10,17 +10,27 @@ const stylish = (diff, padding = 0) => {
     const diffNode = diff[i];
 
     if (diffNode.type === 'LEFT_CHANGED') {
-      const value = lodash.isObject(diffNode.value) ? stylish(diffNode.value, padding + 1) : diffNode.value;
+      const value = lodash.isObject(diffNode.value)
+        ? stylish(diffNode.value, padding + 1)
+        : diffNode.value;
       result += `${spacesPadding}  - ${diffNode.fieldName}: ${value}`;
     } else if (diffNode.type === 'RIGHT_CHANGED') {
-      const value = lodash.isObject(diffNode.value) ? stylish(diffNode.value, padding + 1) : diffNode.value;
+      const value = lodash.isObject(diffNode.value)
+        ? stylish(diffNode.value, padding + 1)
+        : diffNode.value;
       result += `${spacesPadding}  + ${diffNode.fieldName}: ${value}`;
     } else if (diffNode.type === 'NO_CHAGES') {
-      const value = lodash.isObject(diffNode.value) ? stylish(diffNode.value, padding + 1) : diffNode.value;
+      const value = lodash.isObject(diffNode.value)
+        ? stylish(diffNode.value, padding + 1)
+        : diffNode.value;
       result += `${spacesPadding}    ${diffNode.fieldName}: ${value}`;
     } else if (diffNode.type === 'BOTH_CHANGED') {
-      const valueLeft = lodash.isObject(diffNode.valueLeft) ? stylish(diffNode.valueLeft, padding + 1) : diffNode.valueLeft;
-      const valueRight = lodash.isObject(diffNode.valueRight) ? stylish(diffNode.valueRight, padding + 1) : diffNode.valueRight;
+      const valueLeft = lodash.isObject(diffNode.valueLeft)
+        ? stylish(diffNode.valueLeft, padding + 1)
+        : diffNode.valueLeft;
+      const valueRight = lodash.isObject(diffNode.valueRight)
+        ? stylish(diffNode.valueRight, padding + 1)
+        : diffNode.valueRight;
       result += `${spacesPadding}  - ${diffNode.fieldName}: ${valueLeft}\n`;
       result += `${spacesPadding}  + ${diffNode.fieldName}: ${valueRight}`;
     }
