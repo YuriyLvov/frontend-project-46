7d55a80b95f1d67242c5668c02e9388b173d5794
Yuriy Lvov <y.lvov.ca@gmail.com>
2023-03-16 21:15:38 -0400

    update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import parsers from './parsers.js';
 import buildTree from './buildTree.js';
 import getFormatter from './formatter/index.js';
 
-const gendiff = (filepath1, filepath2, formatName = 'stylish') => {
+const gendiff = (filepath1, filepath2, formatName) => {
   const file1 = parsers(filepath1);
   const file2 = parsers(filepath2);
   const formatter = getFormatter(formatName);
