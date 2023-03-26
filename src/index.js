24b0c5d316993d67592a82aa807c7a54f3fc39e5
Yuriy Lvov <y.lvov.ca@gmail.com>
2023-03-26 13:50:20 -0400

    debug

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ const gendiff = (filepath1, filepath2, formatName = 'stylish') => {
   const file1 = parsers(filepath1);
   const file2 = parsers(filepath2);
   const formatter = getFormatter(formatName);
-  console.log('file1', file1);
-  console.log('file2', file2);
+  console.log('file1', JSON.stringify(file1));
+  console.log('file2', JSON.stringify(file2));
   const diff = buildTree(file1, file2);
   const result = formatter(diff);
 
