ae3536c0b3e1ced49ca88c63f3e347a181209117
Yuriy Lvov <y.lvov.ca@gmail.com>
2023-03-26 15:17:37 -0400

    refactored getDiff

--- src/buildTree.js
+++ src/buildTree.js
@@ -1,5 +1,16 @@
 import lodash from 'lodash';
 
+const getValueRecursevly = (value1, value2) => {
+  if (lodash.isPlainObject(value1) && lodash.isPlainObject(value2)) {
+    // eslint-disable-next-line no-use-before-define
+    return getDiff(value1, value2);
+  }
+  const caseValue = value1 || value2;
+
+  // eslint-disable-next-line no-use-before-define
+  return lodash.isPlainObject(caseValue) ? getDiff(caseValue, null) : caseValue;
+};
+
 const getDiff = (first, second) => {
   const allKeys = [...lodash.keys(first), ...lodash.keys(second)];
   const uniqueKeys = lodash.sortBy(lodash.uniq(allKeys));
@@ -12,42 +23,23 @@ const getDiff = (first, second) => {
     const value2Exists = value2 !== undefined; const value1IsObject = lodash.isObject(value1);
     const value2IsObject = lodash.isObject(value2);
 
-    if (first === null || second === null) {
-      const caseValue = first === null ? value2 : value1; return {
+    if (
+      (first === null || second === null)
+      || value1 === value2
+      || (value1IsObject && value2IsObject)
+    ) {
+      return {
         fieldName: key,
         type: 'NO_CHANGES',
-        value: lodash.isObject(caseValue) ? getDiff(caseValue, null) : caseValue,
+        value: getValueRecursevly(value1, value2),
       };
     }
 
     if (!value1Exists || !value2Exists) {
-      const caseValue = value1Exists ? value1 : value2; return {
-        fieldName: key,
-        type: value1Exists ? 'LEFT_CHANGED' : 'RIGHT_CHANGED',
-        value: lodash.isObject(caseValue) ? getDiff(caseValue, null) : caseValue,
-      };
-    }
-
-    if (!value1IsObject && !value2IsObject) {
-      // 100% both are primitives
-      return value1 === value2 ? {
-        fieldName: key,
-        type: 'NO_CHANGES',
-        value: value1,
-      } : {
-        fieldName: key,
-        type: 'BOTH_CHANGED',
-        valueLeft: value1,
-        valueRight: value2,
-      };
-    }
-
-    if (value1IsObject && value2IsObject) {
-      // 100% both are objects
       return {
         fieldName: key,
-        type: 'NO_CHANGES',
-        value: getDiff(value1, value2),
+        type: value1Exists ? 'LEFT_CHANGED' : 'RIGHT_CHANGED',
+        value: getValueRecursevly(value1, value2),
       };
     }
 
@@ -58,6 +50,7 @@ const getDiff = (first, second) => {
       valueRight: value2IsObject ? getDiff(value2, null) : value2,
     };
   });
+
   return result;
 };
 
